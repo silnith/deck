25fe1615a4feb0ec69ef0592d797319595f2677d
kent <silnith@gmail.com>
2016-01-20 16:14:34 -0800

    Initial import of the project from the now-defunct SVN server.

---
+++ src/main/java/org/silnith/deck/Suit.java
@@ -0,0 +1,92 @@
+package org.silnith.deck;
+
+/**
+ * A suit for a playing card.
+ * 
+ * @author <a href="mailto:silnith@gmail.com">Kent Rosenkoetter</a>
+ */
+public enum Suit {
+	CLUB(Color.BLACK),
+	DIAMOND(Color.RED),
+	HEART(Color.RED),
+	SPADE(Color.BLACK);
+
+	/**
+	 * A color for a playing card&#8217;s suit.
+	 * 
+	 * @author <a href="mailto:silnith@gmail.com">Kent Rosenkoetter</a>
+	 */
+	public enum Color {
+		BLACK,
+		RED
+	}
+
+	private final Color color;
+
+	private Suit(final Color color) {
+		this.color = color;
+	}
+
+	/**
+	 * Returns the color of a particular suit.
+	 * 
+	 * @return the color of the suit
+	 */
+	public Color getColor() {
+		return color;
+	}
+
+	/**
+	 * Returns the Unicode symbol for the card suit.
+	 * 
+	 * <table>
+	 *   <thead>
+	 *     <tr>
+	 *       <th>Suit</th>
+	 *       <th>Symbol</th>
+	 *     </tr>
+	 *   </thead>
+	 *   <tbody>
+	 *     <tr>
+	 *       <td>{@link #CLUB}</td>
+	 *       <td>&#x2663;</td>
+	 *     </tr>
+	 *     <tr>
+	 *       <td>{@link #DIAMOND}</td>
+	 *       <td>&#x2666;</td>
+	 *     </tr>
+	 *     <tr>
+	 *       <td>{@link #HEART}</td>
+	 *       <td>&#x2665;</td>
+	 *     </tr>
+	 *     <tr>
+	 *       <td>{@link #SPADE}</td>
+	 *       <td>&#x2660;</td>
+	 *     </tr>
+	 *   </tbody>
+	 * </table>
+	 * 
+	 * @return a single-character Unicode symbol
+	 */
+	public String toSymbol() {
+		switch (this) {
+		case CLUB: return "\u2663";
+		case DIAMOND: return "\u2666";
+		case HEART: return "\u2665";
+		case SPADE: return "\u2660";
+		default: throw new NullPointerException();
+		}
+	}
+
+	@Override
+	public String toString() {
+		switch (this) {
+		case CLUB: return "Club";
+		case DIAMOND: return "Diamond";
+		case HEART: return "Heart";
+		case SPADE: return "Spade";
+		default: return super.toString();
+		}
+	}
+
+}
---
+++ src/test/java/org/silnith/deck/CardTest.java
@@ -0,0 +1,117 @@
+package org.silnith.deck;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.silnith.deck.Card;
+import org.silnith.deck.Suit;
+import org.silnith.deck.Value;
+
+public class CardTest {
+
+	@Test
+	public void testCard() {
+		new Card(Value.EIGHT, Suit.HEART);
+	}
+
+	@Test(expected=NullPointerException.class)
+	public void testCardNullValue() {
+		new Card(null, Suit.HEART);
+	}
+
+	@Test(expected=NullPointerException.class)
+	public void testCardNullSuit() {
+		new Card(Value.EIGHT, null);
+	}
+
+	@Test(expected=NullPointerException.class)
+	public void testCardNullValueAndSuit() {
+		new Card(null, null);
+	}
+
+	@Test
+	public void testGetValue() {
+		final Card card = new Card(Value.EIGHT, Suit.HEART);
+		
+		assertEquals(Value.EIGHT, card.getValue());
+	}
+
+	@Test
+	public void testGetSuit() {
+		final Card card = new Card(Value.EIGHT, Suit.HEART);
+		
+		assertEquals(Suit.HEART, card.getSuit());
+	}
+
+	@Test
+	public void testEquals() {
+		final Card card1 = new Card(Value.EIGHT, Suit.HEART);
+		final Card card2 = new Card(Value.EIGHT, Suit.HEART);
+		
+		assertTrue(card1.equals(card2));
+	}
+
+	@Test
+	public void testHashCode() {
+		final Card card1 = new Card(Value.EIGHT, Suit.HEART);
+		final Card card2 = new Card(Value.EIGHT, Suit.HEART);
+		
+		assertEquals(card1.hashCode(), card2.hashCode());
+	}
+
+	@Test
+	public void testEqualsDifferentValue() {
+		final Card card1 = new Card(Value.EIGHT, Suit.HEART);
+		final Card card2 = new Card(Value.SEVEN, Suit.HEART);
+		
+		assertFalse(card1.equals(card2));
+	}
+
+	@Test
+	public void testHashCodeDifferentValue() {
+		final Card card1 = new Card(Value.EIGHT, Suit.HEART);
+		final Card card2 = new Card(Value.SEVEN, Suit.HEART);
+		
+		assertFalse(card1.hashCode() == card2.hashCode());
+	}
+
+	@Test
+	public void testEqualsDifferentSuit() {
+		final Card card1 = new Card(Value.EIGHT, Suit.HEART);
+		final Card card2 = new Card(Value.EIGHT, Suit.DIAMOND);
+		
+		assertFalse(card1.equals(card2));
+	}
+
+	@Test
+	public void testHashCodeDifferentSuit() {
+		final Card card1 = new Card(Value.EIGHT, Suit.HEART);
+		final Card card2 = new Card(Value.EIGHT, Suit.DIAMOND);
+		
+		assertFalse(card1.hashCode() == card2.hashCode());
+	}
+
+	@Test
+	public void testEqualsDifferentValueAndSuit() {
+		final Card card1 = new Card(Value.EIGHT, Suit.HEART);
+		final Card card2 = new Card(Value.SEVEN, Suit.DIAMOND);
+		
+		assertFalse(card1.equals(card2));
+	}
+
+	@Test
+	public void testHashCodeDifferentValueAndSuit() {
+		final Card card1 = new Card(Value.EIGHT, Suit.HEART);
+		final Card card2 = new Card(Value.SEVEN, Suit.DIAMOND);
+		
+		assertFalse(card1.hashCode() == card2.hashCode());
+	}
+
+	@Test
+	public void testEqualsDifferentType() {
+		final Card card = new Card(Value.EIGHT, Suit.HEART);
+		
+		assertFalse(card.equals("Eight of Hearts"));
+	}
+
+}
